85a394a478488f278fb4f6eb70b9ca2048f06ad5
JongHyeon Park <67833043+Jshman@users.noreply.github.com>
2025-07-21 20:19:23 +0900

    Title: 작은 구슬의 이동, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250721/작은 구슬의 이동/small-marble-movement.py
+++ 250721/작은 구슬의 이동/small-marble-movement.py
@@ -7,20 +7,20 @@ dirc = {'U':'D', 'R':'L', 'D':'U', 'L':'R'}
 
 for _ in range(t):
     if d == 'U':
-        if y == n:
+        if y == 1:
             d = dirc[d]
             continue
-        y += 1
+        y -= 1
     elif d == 'R':
         if x == n:
             d = dirc[d]
             continue
         x += 1
     elif d == 'D':
-        if y == 1:
+        if y == n:
             d = dirc[d]
             continue
-        y -= 1
+        y += 1
     elif d == 'L':
         if x == 1:
             d = dirc[d]
