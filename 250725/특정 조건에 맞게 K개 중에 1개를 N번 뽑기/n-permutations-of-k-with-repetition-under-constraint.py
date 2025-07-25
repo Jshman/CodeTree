b9c87f5bd4eeda925f13553202f47c6a7d12451c
JongHyeon Park <67833043+Jshman@users.noreply.github.com>
2025-07-25 13:30:13 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 333ms, Memory: 27MB, Status: Passed - Codetree

--- 250725/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ 250725/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -1,15 +1,23 @@
 K, N = map(int, input().split())
 
-# Please write your code here.
+def is_same(array):
+    pivot = array[0]
+    for i in range(1, len(array)):
+        if pivot != array[i]:
+            return False
+    return True
+
+# 연속하여 같은 숫자가 3번 이상 X
 def sol(array=[], limit_num=K, limit_length=N):
     if len(array) == limit_length:
-        if len(set(array)) != 1 or len(array) == 1:
-            print(*array)
+        print(*array)
         return
     
-    for i in range(1, K+1):
+    for i in range(1, limit_num+1):
         array.append(i)
-        sol(array, limit_num, limit_length)
+        # 연속성 검사
+        if not (len(array) >= 3 and is_same(array[-3:])):
+            sol(array)
         array.pop()
     return
 
