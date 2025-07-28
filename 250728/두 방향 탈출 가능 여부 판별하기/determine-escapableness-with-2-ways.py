042f2ca5f0fd45d7dda7d07150ee32f6ba5187a2
JongHyeon Park <67833043+Jshman@users.noreply.github.com>
2025-07-28 19:13:35 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1094ms, Memory: 18MB, Status: Time Limit Exceed - Codetree

---
+++ 250728/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,19 @@
+def in_range(y, x, h, w):
+    return 0<=y and y<h and 0<=x and x<w
+
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+dyxs = [(0, 1), (1, 0)]
+stk = [(0, 0)]
+while stk:
+    y, x = stk.pop()
+    grid[y][x] = -1
+    for dy, dx in dyxs:
+        ny, nx = y+dy, x+dx
+        if in_range(ny, nx, n, m) and grid[ny][nx]:
+            stk.append((ny, nx))
+print(1 if grid[n-1][m-1] == -1 else 0)
+
+
+
