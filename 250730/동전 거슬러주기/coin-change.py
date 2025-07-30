2362ac74985f247b030f1fd8bbe09806c4696cf9
JongHyeon Park <67833043+Jshman@users.noreply.github.com>
2025-07-30 19:36:00 +0900

    Title: 동전 거슬러주기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250730/동전 거슬러주기/coin-change.py
+++ 250730/동전 거슬러주기/coin-change.py
@@ -3,11 +3,12 @@ coin = list(map(int, input().split()))
 
 dp = [float('inf')] * (M+1)
 for c in coin:
-    if c < M:
+    if c <= M:
         dp[c] = 1
 
 for i in range(1, M+1):
     for c in coin:
         if i >= c:
             dp[i] = min(dp[i], dp[i - c] + 1)
+
 print(dp[M])
