fd7aab180da3dfd4832fb0ea36f366511b7451bf
JongHyeon Park <67833043+Jshman@users.noreply.github.com>
2025-08-03 11:44:30 +0900

    Title: 증가했다가 감소하는 수열, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250803/증가했다가 감소하는 수열/increasing-and-descreasing-sequence.py
@@ -0,0 +1,8 @@
+n = int(input())
+points = list(map(int, input().split()))
+
+ans = 1 if points[0] < points[-1] else 0
+for i in range(1, n):
+    if points[i] < points[-1]:
+        ans *= 3
+print(ans)
